f6d50389a51aa534c8c85a1b94e1d400c84e0927
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-08-01 14:51:39 -0400

    Create Jsx.js

---
+++ JSX/Jsx.js
@@ -0,0 +1,5 @@
+/*JSX stands for JavaScript Syntax Extension, 
+and it lets you use HTML-like syntax in your React components.
+A function called React.createElement() enables us to create JSX elements in React.
+It accepts three arguments – the HTML element, an object containing the HTML element’s attribute, and the content of the HTML element.*/
+
