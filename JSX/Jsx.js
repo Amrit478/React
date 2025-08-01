254471d5badb28e2ffc997bf4248ff83d3db216b
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-08-01 15:20:37 -0400

    Update Jsx.js
    Hope come back tommrow and check what i did right or wrong and than do it again

--- JSX/Jsx.js
+++ JSX/Jsx.js
@@ -1,5 +1,20 @@
 /*JSX stands for JavaScript Syntax Extension, 
 and it lets you use HTML-like syntax in your React components.
 A function called React.createElement() enables us to create JSX elements in React.
-It accepts three arguments – the HTML element, an object containing the HTML element’s attribute, and the content of the HTML element.*/
+It accepts three arguments – the HTML element, an object containing the HTML element’s attribute, and the content of the HTML element.
+JSX is JavaScript XML, which allows us to write HTML in React. It introduces XML-like tags and attributes to create React elements.
+It makes it easy to create React Components by letting you write HTML-like code in .js*/
 
+//Example is given here for jsx when changing the style of HTML in the components section without adding or changing much code
+
+fucntion Application() {
+  return (
+    <div>
+  <h2 classname="heading"> This is my name </h2>
+    <form classname="Adding"> Choose one form out of these</form>
+  <p> This is whole paragraph including messages
+    </div>
+);
+}
+
+/*Hope come back tommrow and check what i did right or wrong and than do it again*/
