2c231b0db592e8653f53e10f2bb56a9bce0b810a
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-08-01 14:53:18 -0400

    Update Functionalcomponents.js
    done with functional components and come back and learn this again

--- COMPONENTS/Functionalcomponents.js
+++ COMPONENTS/Functionalcomponents.js
@@ -24,3 +24,23 @@ function App() {
     </div>
   );
 }
+/*. Components
+Components are small bit of reusable code that return a React element to be rendered on a webpage.
+It is a group of code that make up a single part of the webpage like buttons, navbar, cards, etc. 
+It is just like a JavaScript function but returns a rendered element. It accepts parameters called "Props".
+Components are named with capital case.
+
+Example Of Functional Component
+
+
+
+function Heading(props) {
+  return <h1>Join us, {props.name}!</h1>;
+}
+
+
+Note:
+
+Functional Components are recommended instead of Class based.
+Functional components are often called statefull components when the UI is updated dynamically due to state's value not Prop's value.
+Functional components are called stateless components when it uses Prop's value to dynamically change UI not state's value.*/
